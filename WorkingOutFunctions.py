687431aa13cbeb4527662a0d2fa9a07631551d3f
Stoove <stoove@live.com>
2016-02-05 14:42:04 +0000

    Playing with function handles
    Added WorkingOutFunctions.py - a short script to work out how to put
    together nested functions (by passing function handles), a technique I
    love to use in other languages.

---
+++ WorkingOutFunctions.py
@@ -0,0 +1,18 @@
+## Short script to explore some of the processes involved in defining, calling, and passing a function.
+## The idea was to learn how to pass a function to another function, as well as learning how to obtain input
+## from the keyboard/console.
+
+## Low level function, representing some type of objective function one might wish to vary.
+def myFunc(x,y):
+    return x*y
+
+## High level function, doing something using the input function.
+def NestedFunc(fun):
+    x = float(input('Please define X: '))
+    y = float(input('Please define Y: '))
+    print('Function of (' + str(x) + ',' + str(y) + ') = ' + str(fun(x,y)))
+    return
+
+## Call the high level function and give it the handle of the low level function for it to call.
+NestedFunc(myFunc)
+
